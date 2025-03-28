227ee67d677e873c7a73fcc84a3512ebc2159775
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2025-03-28 19:11:29 +0800

    chore: actian ingres jdbc test setup

--- src/main/kotlin/Database.kt
+++ src/main/kotlin/Database.kt
@@ -1,7 +1,7 @@
 package ph.dsi.srms
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
+//import com.zaxxer.hikari.HikariConfig
+//import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
 
@@ -12,12 +12,17 @@ fun Application.configureDatabase(): Database {
     val dbPassword = environment.config.property("db.password").getString()
     val dbName = environment.config.property("db.name").getString()
 
-    val config = HikariConfig().apply {
-        jdbcUrl = "jdbc:ingres://localhost:AX7/srmsdb"
-        driverClassName = "com.ingres.jdbc.IngresDriver"
-        maximumPoolSize = 10
-    }
+//    val config = HikariConfig().apply {
+//        jdbcUrl = "jdbc:ingres://localhost:AX7/srmsdb"
+//        driverClassName = "com.ingres.jdbc.IngresDriver"
+//        maximumPoolSize = 10
+//    }
 
-    val dataSource = HikariDataSource(config)
-    return Database.connect(datasource = dataSource)
+//    val dataSource = HikariDataSource(config)
+    return Database.connect(
+        // url acquired from Actian Director
+        // https://docs.actian.com/ingres/10s/index.html#page/QuickStartWin%2FView_JDBC_Connection_URL_in_Actian_Director.htm%23
+        "jdbc:ingres://LAPTOP-14C4E4AT:AX7/srmsdb;UID=dsource;PWD=DS1@dm1n",
+        driver = "com.ingres.jdbc.IngresDriver"
+    )
 }
